f70a29d68d21cb19c9a3e4dc702bf3371024559f
davidjorge <davidjorge126@gmail.com>
2022-04-19 17:37:49 -0500

    :bricks: [ADD] [MOD]
    Webpack server configuration

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const App = () => {
-    return <h1>Hello world!</h1>;
+    return <h1>Hello World React!, We are learning react</h1>;
 };
 
 export default App;
--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -8,7 +8,7 @@ module.exports = {
         path: path.resolve(__dirname, "dist"),
         filename: "bundle.js",
     },
-    mode: "production",
+    mode: "development",
     resolve: {
         extensions: [".js", ".jsx"],
     },
