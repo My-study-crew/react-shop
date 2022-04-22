de9b00908465d0bb08150013a5496d12bd3cd36e
davidjorge <davidjorge126@gmail.com>
2022-04-22 16:15:50 -0500

    :bricks: [MOD]
    Styles modification and configuration

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import React from "react";
-import ReactDOM from "react-dom";
-import App from "./components/App";
+import ReactDOM from "react-dom/client";
+import App from "./components/App.jsx";
 
-ReactDOM.render(<App />, document.getElementById("app"));
+const container = document.getElementById("root");
+const root = ReactDOM.createRoot(container);
+
+root.render(<App />);
