844a534c270915c1661a216a8a9e24cca3d489a8
davidjorge <davidjorge126@gmail.com>
2022-04-28 16:48:53 -0500

    :art: [ADD] [MOD]
    useRef and form configuration

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,19 +1,31 @@
-import React from "react";
+import React, { useRef } from "react";
 import yardLogo from "@logos/logo_yard_sale.svg";
 import "@styles/Login.scss"; // Style
 
 const Login = () => {
+    const form = useRef(null);
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        const formData = new FormData(form.current);
+        const data = {
+            username: formData.get("email"),
+            password: formData.get("password"),
+        };
+        console.log(data);
+    };
+
     return (
         <div className="login">
             <div className="login-container">
                 <img src={yardLogo} alt="logo" className="logo" />
-                <form action="/" className="form">
+                <form action="/" className="form" ref={form}>
                     <label htmlFor="email" className="label">
                         Email address
                     </label>
                     <input
                         type="text"
-                        id="email"
+                        name="email"
                         placeholder="platzi@example.com"
                         className="input input-email"
                     />
@@ -22,7 +34,7 @@ const Login = () => {
                     </label>
                     <input
                         type="password"
-                        id="password"
+                        name="password"
                         placeholder="*********"
                         className="input input-password"
                     />
@@ -30,6 +42,7 @@ const Login = () => {
                         type="submit"
                         value="Log in"
                         className="primary-button login-button"
+                        onClick={handleSubmit}
                     />
                     <a href="/">Forgot my password</a>
                 </form>
