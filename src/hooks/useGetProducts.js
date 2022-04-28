c82951f43e5f456d8116b6467ec6c764f5dd6cce
davidjorge <davidjorge126@gmail.com>
2022-04-28 15:44:07 -0500

    :art: [ADD] [MOD]
    Custom hook configuration

---
+++ src/hooks/useGetProducts.js
@@ -0,0 +1,15 @@
+import { useEffect, useState } from "react";
+import Axios from "axios";
+
+const useGetProducts = (API) => {
+    const [products, setProducts] = useState([]);
+
+    useEffect(async () => {
+        const response = await Axios(API);
+        setProducts(response.data);
+    }, []);
+
+    return products;
+};
+
+export default useGetProducts;
